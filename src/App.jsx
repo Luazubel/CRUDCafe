d4fef21c1a96580318045cfdc596347fbe498e81
Lucia Azubel <Lucia_24_azubel@hotmail.com>
2023-10-08 16:39:09 -0300

    agregamos varios componentes y el sistema de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Inicio from "./components/views/Inicio"
+import Administrador from "./components/views/Administrador"
+import Error404 from "./components/views/Error404"
+import Menu from "./components/common/Menu"
+import Footer from "./components/common/Footer"
 
 function App() {
   
 
   return (
-    <>
-     <h1>hola</h1>
-    </>
+     //administrar las rutas
+    <BrowserRouter>
+    <Menu></Menu>
+    <Routes>
+      {/* las rutas siempre son el dominio + el path */}
+      <Route exact path="/" element={<Inicio></Inicio>}>
+      </Route>
+      <Route exact path="/administrar" element={<Administrador></Administrador>} />
+      <Route path="*" element={<Error404/>}>
+      </Route>
+    </Routes>
+    <Footer></Footer>
+    </BrowserRouter>
   )
 }
 
