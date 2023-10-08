ff2b771fc483001ff719c710771a9ad5bc3445de
Lucia Azubel <Lucia_24_azubel@hotmail.com>
2023-10-08 17:01:53 -0300

    agregamos los link del menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,9 +1,20 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
     return (
-        <div>
-            <h1>Menu principal</h1>
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Cafe</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/" className={"nav-item nav-link"}>Inicio</NavLink>
+            <NavLink to="/administrar" className={"nav-item nav-link"}>Administrador</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
